d184ab9ad65902128f9d4f0b409781961c2d5c87
RM <30491118+RichMar@users.noreply.github.com>
2023-04-06 13:30:02 +0200

    Update convertshptocsv.py

--- convertshptocsv.py
+++ convertshptocsv.py
@@ -3,7 +3,7 @@ import sys
 from osgeo import ogr, osr, gdal
 import os
 #souradnice S-JTSK (EPSG:5514) prevod na GPS (EPSG:4326)
-shpfile = r'c:\Users\marik.AGIR\OneDrive - Agir AG\Dokumenty\GitHub\ZVS\data\zastavky_JcK_20230306.shp' #sys.argv[1]
+shpfile = r'.\data\zastavky_JcK_20230306.shp' #sys.argv[1]
 csvfile = r'test.csv' #sys.argv[2]
 
 if not os.path.exists(shpfile):
@@ -40,53 +40,30 @@ outSpatialRef.ImportFromEPSG(4326)
 # create the CoordinateTransformation
 coordTrans = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
 
-# # get the output layer's feature definition
-# outLayerDefn = outLayer.GetLayerDefn()
 # get the input layer
 inDataSet = driver.Open(shpfile)
 inLayer = inDataSet.GetLayer()
 # loop through the input features
 inFeature = inLayer.GetNextFeature()
+csvfile = open('zastavky-JCK.csv', 'w')
+csvfile.write("lat;lon;okres;name;typ" + "\n")
+aa = 0
 for feature in layer:
     geom = feature.GetGeometryRef()
     geom.Transform(coordTrans)
+    point = geom.Centroid().ExportToWkt()
+    a = point.find('(')
+    b = point.rfind(' ')
+    c = point.find(')')
+    lat = point[a + 1:b]
+    lon = point[b + 1:c]
+    ref = str(int(feature.GetField("CISLO_NUM")))
     okres = feature.GetField("OKRES")
     poplong = feature.GetField("POPIS_LONG")
     stan = feature.GetField("STANOVISTE")
+    typ = feature.GetField("TYP")
     print(geom.Centroid().ExportToWkt() + str(okres) + " " + str(poplong) + " " + str(stan))
-
-a = 0
-while inFeature:
-    # get the input geometry
-    geom = inFeature.GetGeometryRef()
-    # reproject the geometry
-    geom.Transform(coordTrans)
-    a = a + 1
-    print(str(a) + " -- " + str(geom))
-    # # create a new feature
-    # outFeature = ogr.Feature(outLayerDefn)
-    # # set the geometry and attribute
-    # outFeature.SetGeometry(geom)
-#Get field names
-dfn = layer.GetLayerDefn()
-nfields = dfn.GetFieldCount()
-fields = []
-for i in range(nfields):
-    fields.append(dfn.GetFieldDefn(i).GetName())
-fields.append('kmlgeometry')
-csvwriter = csv.DictWriter(csvfile, fields)
-try:
-    csvwriter.writeheader() #python 2.7+
-except:
-    csvfile.write(','.join(fields)+'\n')
-
-# Write attributes and kml out to csv
-for feat in layer:
-    attributes = feat.items()
-    geom = feat.GetGeometryRef()
-    attributes['kmlgeometry'] = geom.ExportToKML()
-    csvwriter.writerow(attributes)
-
-#clean up
-del csvwriter, layer, ds
+    csvfile.write(lat + ";" + lon + ";" + ref + ";" + okres + ";" + poplong + ";" + typ + "\n")
+    aa += 1
+print("Konec - počet řádků: " + str(aa))
 csvfile.close()
