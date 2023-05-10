625debaa342088008a72a1e32a22f999ae272b19
RM <30491118+RichMar@users.noreply.github.com>
2023-05-10 14:12:11 +0200

    Update searchst.py

--- searchst.py
+++ searchst.py
@@ -2,6 +2,8 @@ import requests
 from math import radians, cos, sin, asin, sqrt
 import os
 import csv
+from itertools import count # izip for maximum efficiency
+from copy import deepcopy
 import difflib
 
 def get_distance(lat_1, lng_1, lat_2, lng_2):  # vypocet vzdalenosti bodu
@@ -16,8 +18,10 @@ def get_distance(lat_1, lng_1, lat_2, lng_2):  # vypocet vzdalenosti bodu
 
   # area[name="Jihočeský kraj"];
   # node(area)["highway"="bus_stop"];
-csvfile = 'zastavky-JCK.csv'
+csvfile = 'zastavky-JCK-bus.csv'
 seznam_st = []
+uzel=[]
+csv_reader_kopie = []
 overpass_url = "http://overpass-api.de/api/interpreter"
 overpass_query = """[out:csv(::lat, ::lon, "official_name", name, "ref:CIS_JR", "ref", "bus", "public_transport",
  ::count, ::id)]; \n ( \n"""
@@ -30,11 +34,11 @@ print(overpass_query)
 
 response = requests.get(overpass_url, params={'data': overpass_query})
 print("encoding :" + response.encoding)
-response.encoding = 'UTF-8'
+response.encoding = 'utf8'
 print("encoding :" + response.encoding)
 data = [row.split('\t') for row in response.text.split('\n')]
 # uloží data z OSM do csv souboru
-with open('OSMzastavkyJCK.csv', 'w', newline='', encoding="UTF-8") as f:
+with open('OSMzastavkyJCK.csv', 'w', newline='', encoding="utf8") as f:
     writer = csv.writer(f, delimiter=',')
     # writer.writerow(["lat", "lon", "official_name", "name", "ref:CIS_JR", "ref", "bus", "public_transport", "count"])
     writer.writerows(data)
@@ -45,15 +49,30 @@ for x in data:
     if "lat" not in str(x) and not x[0] == "":
         seznam_st.append(x)
         print(str(x))
+    # else:
+    #     print('22222222')
+
 # načte scv soubor se zastavákami od kraje
 if os.path.exists(csvfile):
-    csvfile = open(csvfile, 'r')
+    csvfile = open(csvfile, newline='', encoding='utf8')
     csv_reader = csv.reader(csvfile, delimiter=';')
-pocet = 0
+    zastavkykraj = list(csv_reader)
+    pocet = 0
+    # for y in csv_reader:
+    #     csv_reader_kopie.append(y)
+
+#https://stackoverflow.com/questions/11150155/why-cant-i-repeat-the-for-loop-for-csv-reader
+
+    for x in zastavkykraj:
+        # print("x:" + str(x))
+        if ("lat" and "ref") not in x:
+            ref = x[2]
+            print('ref: ' + str(ref))
+            index_zast = [(i, element.index(ref)) for i, element in enumerate(zastavkykraj) if ref in element]
+            print('index_zast: ' + str(index_zast))
 
 
-
-# for x in csv_reader:
+    # for x in csv_reader:
 #     if "lat" not in x:
 #         for y in data:
 #             if "@lat" not in y and not y[0] == "":
@@ -67,11 +86,16 @@ pocet = 0
 #                         print(str(y) + ": " + str(vzd))
 #                         pocet += 1
 
-for x in csv_reader:
+# for x in csv_reader:
+#     print(x)
+#     if ("lat" and "ref") not in x:
+#         ref = x[2]
+#         print("ref: " + str(ref))
+#         index_zast = [(i, element.index(ref)) for i, element in enumerate(csv_reader) if ref in element]
+#         uzel.append(index_zast)
+#         for i in uzel:
+#             print("index: " + str(i))
 
-ref = y[2].replace(" ", "")
-index_les = [(i, element.index(ref)) for i, element in enumerate(body_les_seznam) if ref in element]
-print("pocet zastávek v OSM: " + str(pocet))
 
 
 
