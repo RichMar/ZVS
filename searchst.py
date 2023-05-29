67a57ba6f2670bdcfe44412accd95a27bc1c2983
RM <30491118+RichMar@users.noreply.github.com>
2023-05-29 13:34:06 +0200

    Update searchst.py

--- searchst.py
+++ searchst.py
@@ -63,7 +63,7 @@ def get_keys(bod_od, bod_osm, porov, dstan):
             elif porov > 0.11:
                 jm = bod_od[4]
             elif x == bod_od[5]:
-                jm = ""
+                jm = bod_od[4]
         elif i == 3 and x == "" and bod_od[5] == "":
             jm = bod_od[4]
         # elif i == 3 and x == "" and not bod_od[5] == "":
@@ -85,7 +85,7 @@ def get_keys(bod_od, bod_osm, porov, dstan):
                 problemovybodosm.append(bod_osm)
             else:
                 # locref = bod_od[5]
-                locref = dstan
+                locref = ""
         elif i == 10 and x == "":
             # locref = bod_od[5]
             locref = dstan
@@ -105,9 +105,10 @@ def get_keys(bod_od, bod_osm, porov, dstan):
     edittag[2] = jm
     # edittag[3].append(refe)
     edittag[3] = refe
-    edittag[4] = bod_osm[0]
-    edittag[5] = bod_osm[1]
-    edittag[6] = locref
+    edittag[4] = locref
+    edittag[5] = bod_osm[0]
+    edittag[6] = bod_osm[1]
+
     return edittag
 
 
@@ -211,7 +212,7 @@ def tridit(dlat, dlon, limvzd, dx, dn, dg, pocetz, ddata, dstan, doficialname, d
     minimum = min(vzdalenost)
     if minimum > 0.025:
         # chybejicisinglzast = [[] for i in range(4)]
-        chybejicisinglzast = ['']*4
+        chybejicisinglzast = ['']*5
         chybejicisinglzast[0] = []
         # chybejicisinglzast[0].append(lat)
         chybejicisinglzast[0] = dlat
@@ -224,6 +225,8 @@ def tridit(dlat, dlon, limvzd, dx, dn, dg, pocetz, ddata, dstan, doficialname, d
         chybejicisinglzast[3] = []
         # chybejicisinglzast[3].append(oficialname)
         chybejicisinglzast[3] = doficialname
+        chybejicisinglzast[4] = []
+        chybejicisinglzast[4] = dstan
         chybejicisinglzast_list.append(chybejicisinglzast[:])
                     # dg = 1
                     # pruchod = 0
@@ -504,8 +507,8 @@ print("Total items after deduplication bezdupl_list:", len(bezdupl_list))
 print("Total items in original josm :", len(josm))
 print("Total items after deduplication bezdupl_josm:", len(bezdupl_josm))
 print("Ahoj")
-tisk_csv(bezdupl_list, "bezdupl_list", ["lat", "lon", "ref:CIS_JR", "official_name"])
-tisk_csv(bezdupl_josm, "bezdupl_josm", ["elemnt", "id", "official_name", "ref:CIS_JR", "lat", "lon", "local_ref"])
+tisk_csv(bezdupl_list, "bezdupl_list", ["lat", "lon", "ref:CIS_JR", "official_name", "local_ref"])
+tisk_csv(bezdupl_josm, "bezdupl_josm", ["elemnt", "id", "official_name", "ref:CIS_JR", "local_ref", "lat", "lon"])
 tisk_csv(bezdupl_problemovazast, "problemovazast", ["lat", "lon", "ref", "okres", "name", "stanoviste", "typ"])
 # bod_osm: 0)::lat, 1)::lon, 2)"official_name", 3)name, 4)"ref:CIS_JR", 5)"ref", 6)"bus", 7)"public_transport",8)::count, 9)::id) 10)local_ref
 tisk_csv(bezdupl_problemovybodosm, "problemovybodosm", ["lat", "lon", "ref", "official_name", "name", "ref:CIS_JR",
